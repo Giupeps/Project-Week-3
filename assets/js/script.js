d9c948ff148c07213a585cd0215c87d990a2cc79
Giupeps <dipisagiuseppe92@gmail.com>
2022-09-19 00:14:19 +0200

    try to add error function

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,24 +5,36 @@ function addSymbol(element) {
     document.getElementById('display').value += element
 }
 
-  function back(){
+function back(){
 
    display.value = display.value.substring(0, display.value.length - 1);
 
 }
 
-  function clearAll() {
+function clearAll() {
 
    document.getElementById('display').placeholder= 0
    document.getElementById("display").value = ''
-  }
+}
+
+// function addToDisplay(element) {
+//   let last = document.getElementById('display').innerHTML.charAt(document.getElementById('display').innerHTML.length - 1)
+//     if ((last == '+' || last == '-' || last == '*' || last == '/') && (element= '+' || element == '-' || element == '' || element == '/')) {
+//       document.getElementById('display').innerHTML = 'error'
+//     }
+//     else {
+//         document.getElementById('display').innerHTML += element
+//     }
+// }
+   
   
-  function calc() {
+function calc() {
     
     let result = eval(document.getElementById('display').value)
     document.getElementById('display').value = result
 
-  }
+}
   
- 
+
+
   
